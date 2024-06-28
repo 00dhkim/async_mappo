98b168273cf135822a407add484cdad705d7d72e
Dohyun Kim <00dh.kim@gmail.com>
2024-06-28 15:39:39 +0900

    fix: dependency update for my machine

--- onpolicy/scripts/train_gridworld.sh
+++ onpolicy/scripts/train_gridworld.sh
@@ -11,7 +11,7 @@ echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, ma
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=2 python train/train_gridworld.py \
+    CUDA_VISIBLE_DEVICES=2 python3 train/train_gridworld.py \
     --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} \
     --log_interval 1 --wandb_name "mapping" --user_name "yang-xy20" --num_agents ${num_agents} \
     --num_obstacles ${num_obstacles} --cnn_layers_params '16,7,2,1 32,5,2,1 16,3,1,1' --hidden_size 64 --seed 1 --n_training_threads 1 \
