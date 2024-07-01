550e1768f96f5002d21d90e9d991f9ecb0574c3e
Dohyun Kim <00dh.kim@gmail.com>
2024-07-01 13:19:06 +0900

    .

--- onpolicy/scripts/render_gridworld.sh
+++ onpolicy/scripts/render_gridworld.sh
@@ -10,7 +10,7 @@ seed_max=3
 echo "env is ${env}"
 for seed in `seq ${seed_max}`
 do
-    CUDA_VISIBLE_DEVICES=2 python render/render_gridworld.py\
+    CUDA_VISIBLE_DEVICES=0,1,2,3 python render/render_gridworld.py\
       --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} \
       --num_agents ${num_agents} --num_obstacles ${num_obstacles} \
       --seed ${seed} --n_training_threads 1 --n_rollout_threads 1 --render_episodes 100 \
@@ -18,7 +18,8 @@ do
       --model_dir "./results/GridWorld/MiniGrid-MultiExploration-v0/mappo/async_global_new_attn_para_no_agent_id_overlap_penalty_single_normal50/wandb/run-20220224_140614-3vhpd2ge/files/" \
       --max_steps 200 --use_complete_reward --agent_view_size 7 --local_step_num 1 --use_random_pos \
       --astar_cost_mode utility --grid_goal --goal_grid_size 5 --cnn_trans_layer 1,3,1,1 \
-      --use_stack --grid_size 25 --use_recurrent_policy --use_stack --use_global_goal --use_overlap_penalty --use_eval --wandb_name "mapping" --user_name "yang-xy20" --asynch & 
+      --use_stack --grid_size 25 --use_recurrent_policy --use_stack --use_global_goal --use_overlap_penalty --use_eval --wandb_name "mapping" --user_name "yang-xy20" --asynch \
+      --use_wandb false
 done
 
 # --use-render
--- onpolicy/scripts/train_gridworld.sh
+++ onpolicy/scripts/train_gridworld.sh
@@ -11,12 +11,13 @@ echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, ma
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=2 python3 train/train_gridworld.py \
+    CUDA_VISIBLE_DEVICES=0,1,2,3 python3 train/train_gridworld.py \
     --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} \
     --log_interval 1 --wandb_name "mapping" --user_name "yang-xy20" --num_agents ${num_agents} \
     --num_obstacles ${num_obstacles} --cnn_layers_params '16,7,2,1 32,5,2,1 16,3,1,1' --hidden_size 64 --seed 1 --n_training_threads 1 \
     --n_rollout_threads 50 --num_mini_batch 1 --num_env_steps 80000000 --ppo_epoch 3 --gain 0.01 \
     --lr 5e-4 --critic_lr 5e-4 --max_steps 150 --use_complete_reward --agent_view_size 7 --local_step_num 5 --use_random_pos \
     --astar_cost_mode normal  --cnn_trans_layer 1,3,1,1 --grid_size 25 --use_recurrent_policy \
-    --use_global_goal --use_overlap_penalty --use_stack --goal_grid_size 5 --use_discrect --asynch
+    --use_global_goal --use_overlap_penalty --use_stack --goal_grid_size 5 --use_discrect --asynch \
+    --use_wandb false
 done
